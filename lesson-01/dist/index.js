fb8cfb5800c187bc0d5971551764140c3ed89a76
tarunclub <tarunkm__@outlook.com>
2024-03-15 23:09:44 +0530

    get balance

--- lesson-01/dist/index.js
+++ lesson-01/dist/index.js
@@ -1,9 +1,22 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const helpers_1 = require("@solana-developers/helpers");
+const web3_js_1 = require("@solana/web3.js");
 require("dotenv/config");
-// const keypair = Keypair.generate();
-// console.log('The public key is: ', keypair.publicKey.toBase58());
-// console.log('The private key is: ', keypair.secretKey);
 const keypair1 = (0, helpers_1.getKeypairFromEnvironment)('SECRET_KEY');
 console.log(keypair1.publicKey.toBase58());
+const connection = new web3_js_1.Connection((0, web3_js_1.clusterApiUrl)('devnet'));
+const address = new web3_js_1.PublicKey('GteaL8FV2uLrdARiH4SK3y3EkDnnmcMe9rxasZSvA6vD');
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const balance = yield connection.getBalance(address);
+    console.log(`The balance of the account ${address} is ${balance / web3_js_1.LAMPORTS_PER_SOL}`);
+}))();
--- lesson-01/src/index.ts
+++ lesson-01/src/index.ts
@@ -1,12 +1,21 @@
-import { Keypair } from '@solana/web3.js';
 import { getKeypairFromEnvironment } from '@solana-developers/helpers';
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  clusterApiUrl,
+} from '@solana/web3.js';
 import 'dotenv/config';
 
-const keypair = Keypair.generate();
-
-console.log('The public key is: ', keypair.publicKey.toBase58());
-console.log('The private key is: ', keypair.secretKey);
-
 const keypair1 = getKeypairFromEnvironment('SECRET_KEY');
-
 console.log(keypair1.publicKey.toBase58());
+
+const connection = new Connection(clusterApiUrl('devnet'));
+const address = new PublicKey('GteaL8FV2uLrdARiH4SK3y3EkDnnmcMe9rxasZSvA6vD');
+
+(async () => {
+  const balance = await connection.getBalance(address);
+  console.log(
+    `The balance of the account ${address} is ${balance / LAMPORTS_PER_SOL}`
+  );
+})();
